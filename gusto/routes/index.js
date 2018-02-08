4c21210451f7932cdbb4969f2f76bdabcc25eee9
Edan Lewis <edan@lewisarts.com>
2018-02-07 16:15:58 -0800

    Create store model and controller. Add functionality to save to db.

--- gusto/routes/index.js
+++ gusto/routes/index.js
@@ -1,18 +1,10 @@
 const express = require('express');
 const router = express.Router();
+const storeController = require('../controllers/storeController');
+const { catchErrors } = require('../handlers/errorHandlers');
 
-// Do work here
-router.get('/', (req, res) => {
-  const edan = { name: 'Edan', age: 24, coolness: 1000000 };
-  // res.json(edan);
-  // res.send('Hey! It works!');
-  res.render('hello');
-});
-
-router.get('/reverse/:name', (req, res) => {
-  const reverse = [...req.params.name].reverse().join('');
-  res.send(reverse);
-  
-});
+router.get('/', storeController.homePage);
+router.get('/add', storeController.addStore);
+router.post('/add', catchErrors(storeController.createStore));
 
 module.exports = router;
