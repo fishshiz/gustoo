632e8242f67870e48d72dba827405fada6445690
Edan Lewis <edan@lewisarts.com>
2018-02-11 15:16:13 -0800

    Adds geolocation through google maps

--- gusto/controllers/storeController.js
+++ gusto/controllers/storeController.js
@@ -11,7 +11,27 @@ exports.addStore = (req, res) => {
 };
 
 exports.createStore = async (req, res) => {
-    const store = new Store(req.body);
-    await store.save();
-    res.redirect('/');
+    const store = await(new Store(req.body)).save();
+    req.flash('success', `Succesfully created ${store.name}! Care to leave a review?`);
+    res.redirect(`/stores/${store.slug}`);
 };
+
+exports.getStores = async (req, res) => {
+    const stores = await Store.find();
+    res.render('stores', { title: 'Stores', stores });
+}
+
+exports.editStore = async (req, res) => {
+    const store = await Store.findOne({ _id: req.params.id });
+    res.render('editStore', { title: `Edit ${store.name}`, store});
+}
+
+exports.updateStore = async (req, res) => {
+    req.body.location.type = 'Point';
+    const store = await Store.findOneAndUpdate({ _id: req.params.id }, req.body, {
+        new: true,
+        runValidators: true
+    }).exec();
+    req.flash('success', `Successfully updated ${store.name}. <a href="store/${store.slug}>View Store</a>`)
+    res.redirect(`/stores/${store._id}/edit`)
+}
--- gusto/models/Store.js
+++ gusto/models/Store.js
@@ -1,29 +1,49 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 mongoose.Promise = global.Promise;
-const slug = require('slugs');
+const slug = require("slugs");
 
 const storeSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        trim: true,
-        required: 'Please include a store name!'
+  name: {
+    type: String,
+    trim: true,
+    required: "Please include a store name!"
+  },
+  slug: String,
+  description: {
+    type: String,
+    trim: true
+  },
+  tags: [String],
+  created: {
+    type: Date,
+    default: Date.now
+  },
+  location: {
+    type: {
+      type: String,
+      default: "Point"
     },
-    slug: String,
-    description: {
-        type: String,
-        trim: true
-    },
-    tags: [String]
-});
-
-storeSchema.pre('save', function(next) {
-    if(!this.isModified('name')) {
-        next();
-        return;
+    coordinates: [
+      {
+        type: Number,
+        required: "Must supply coordinates."
+      }
+    ],
+    address: {
+      type: String,
+      required: "Please include an address!"
     }
-    this.slug = slug(this.name);
-    next();
-    // TODO make slug more resilient for repeat names.
+  }
 });
 
-module.exports = mongoose.model('Store', storeSchema);
+storeSchema.pre("save", function(next) {
+  if (!this.isModified("name")) {
+    next();
+    return;
+  }
+  this.slug = slug(this.name);
+  next();
+  // TODO make slug more resilient for repeat names.
+});
+
+module.exports = mongoose.model("Store", storeSchema);
