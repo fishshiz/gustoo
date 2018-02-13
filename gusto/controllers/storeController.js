bc7811da2ff90ff081e19bdb77c6d270beb7df13
Edan Lewis <edan@lewisarts.com>
2018-02-12 16:14:40 -0800

    Adds store show page

--- gusto/controllers/storeController.js
+++ gusto/controllers/storeController.js
@@ -1,5 +1,19 @@
 const mongoose = require('mongoose');
 const Store = mongoose.model('Store');
+const multer = require('multer');
+const jimp = require('jimp');
+const uuid = require('uuid');
+const multerOptions = {
+    storage: multer.memoryStorage(),
+    fileFilter(req, file, next) {
+        const isPhoto = file.mimetype.startsWith('image/');
+        if(isPhoto) {
+            next(null, true);
+        } else {
+            next({ message: 'That filetype isn\'t allowed!' }, false);
+        }
+    }
+}
 
 exports.homePage = (req, res) => {
     console.log(req.name);
@@ -10,6 +24,21 @@ exports.addStore = (req, res) => {
     res.render('editStore', { title: 'Add Store' });
 };
 
+exports.upload = multer(multerOptions).single('photo');
+
+exports.resize = async (req, res, next) => {
+    if(!req.file) {
+        next();
+        return;
+    }
+    const extension = req.file.mimetype.split('/')[1];
+    req.body.photo = `${uuid.v4()}.${extension}`;
+    const photo = await jimp.read(req.file.buffer);
+    await photo.resize(800, jimp.AUTO);
+    await photo.write(`public/uploads/${req.body.photo}`);
+    next();
+}
+
 exports.createStore = async (req, res) => {
     const store = await(new Store(req.body)).save();
     req.flash('success', `Succesfully created ${store.name}! Care to leave a review?`);
@@ -19,6 +48,12 @@ exports.createStore = async (req, res) => {
 exports.getStores = async (req, res) => {
     const stores = await Store.find();
     res.render('stores', { title: 'Stores', stores });
+}
+
+exports.getStore = async (req, res, next) => {
+    const store = await Store.findOne({ slug: req.params.slug })
+    if(!store) return next();
+    res.render('store', { store });
 }
 
 exports.editStore = async (req, res) => {
@@ -32,6 +67,6 @@ exports.updateStore = async (req, res) => {
         new: true,
         runValidators: true
     }).exec();
-    req.flash('success', `Successfully updated ${store.name}. <a href="store/${store.slug}>View Store</a>`)
-    res.redirect(`/stores/${store._id}/edit`)
+    req.flash('success', `Successfully updated <strong>${store.name}</strong>. <a href="/stores/${store.slug}">View Store</a>`)
+    res.redirect(`/stores`)
 }
