bdc2f1c6e57257ad46ccf0ce7826cfbcdc27d3d4
Edan Lewis <edan@lewisarts.com>
2018-02-20 19:29:37 -0800

    Adds password reset functionality

--- gusto/routes/index.js
+++ gusto/routes/index.js
@@ -1,40 +1,50 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const storeController = require('../controllers/storeController');
-const userController = require('../controllers/userController');
-const authController = require('../controllers/authController');
-const { catchErrors } = require('../handlers/errorHandlers');
+const storeController = require("../controllers/storeController");
+const userController = require("../controllers/userController");
+const authController = require("../controllers/authController");
+const { catchErrors } = require("../handlers/errorHandlers");
 
-router.get('/', catchErrors(storeController.getStores));
-router.get('/stores', catchErrors(storeController.getStores));
-router.get('/add', authController.checkLogin, storeController.addStore);
-router.post('/add',
-    storeController.upload,
-    catchErrors(storeController.resize),
-    catchErrors(storeController.createStore)
+router.get("/", catchErrors(storeController.getStores));
+router.get("/stores", catchErrors(storeController.getStores));
+router.get("/add", authController.checkLogin, storeController.addStore);
+router.post(
+  "/add",
+  storeController.upload,
+  catchErrors(storeController.resize),
+  catchErrors(storeController.createStore)
 );
-router.post('/add/:id',
-    storeController.upload,
-    catchErrors(storeController.resize),
-    catchErrors(storeController.updateStore)
+router.post(
+  "/add/:id",
+  storeController.upload,
+  catchErrors(storeController.resize),
+  catchErrors(storeController.updateStore)
 );
-router.get('/stores/:slug', catchErrors(storeController.getStore));
-router.get('/stores/:id/edit', catchErrors(storeController.editStore));
-router.get('/tags', catchErrors(storeController.getStoresByTag));
-router.get('/tags/:tag', catchErrors(storeController.getStoresByTag));
-router.get('/login', userController.loginForm);
-router.post('/login', authController.login);
-router.get('/register', userController.registerForm);
+router.get("/stores/:slug", catchErrors(storeController.getStore));
+router.get("/stores/:id/edit", catchErrors(storeController.editStore));
+router.get("/tags", catchErrors(storeController.getStoresByTag));
+router.get("/tags/:tag", catchErrors(storeController.getStoresByTag));
+router.get("/login", userController.loginForm);
+router.post("/login", authController.login);
+router.get("/register", userController.registerForm);
 
-router.post('/register',
+router.post(
+  "/register",
   userController.validateRegister,
   userController.register,
   authController.login
 );
 
-router.get('/logout', authController.logout);
+router.get("/logout", authController.logout);
 
-router.get('/account', authController.checkLogin, userController.account);
-router.post('/account', catchErrors(userController.updateAccount));
+router.get("/account", authController.checkLogin, userController.account);
+router.post("/account", catchErrors(userController.updateAccount));
+router.post("/account/forgot", catchErrors(authController.forgot));
+router.get("/account/reset/:token", catchErrors(authController.reset));
+router.post(
+  "/account/reset/:token",
+  authController.confirmedPasswords,
+  catchErrors(authController.update)
+);
 
 module.exports = router;
