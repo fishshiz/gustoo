3b5e8595e7192e6bf4c6addc03c73bda1170e64c
Edan Lewis <edan@lewisarts.com>
2018-02-28 17:21:23 -0800

    plots stores w google maps api

--- gusto/routes/index.js
+++ gusto/routes/index.js
@@ -46,9 +46,11 @@ router.post(
   authController.confirmedPasswords,
   catchErrors(authController.update)
 );
+router.get('/map', storeController.mapPage);
 
 // API endpoints
 
 router.get("/api/search", catchErrors(storeController.searchStores));
+router.get('/api/stores/near', catchErrors(storeController.mapStores));
 
 module.exports = router;
